017b8ee93fbb3e8334ba68cb15cba1fd8166f10c
Trubulica <tamaramartinovichr@gmail.com>
2023-01-27 20:26:36 +0100

    I have rewritten the If/Elif/Else statements thus removing repetitive code.

--- Rock_Paper_Scissors.py
+++ Rock_Paper_Scissors.py
@@ -1,12 +1,13 @@
 import random
 
-game = {1 : "Rock", 2 : "Paper", 3 : "Scissors", 4 : "Exit game"}
 print("""\tWelcome to a game of Rock, Paper, Scissors! \n
         Please choose one of the following options: \n
         1. Rock
         2. Paper
         3. Scissors
         4. Exit\n""")
+
+game = {1 : "Rock", 2 : "Paper", 3 : "Scissors", 4 : "Exit game"}
 
 while True:
 
@@ -22,29 +23,12 @@ while True:
     elif user_input >= 5:
         print("Please choose a number from 1 to 4!")
 
-    if computer_choice == 1:
-        if user_input == 1:
-            print("Draw!")
-        elif user_input == 2:
-            print(f"Player wins,", user_move, "beats", computer_move)
-        elif user_input == 3:
-            print(f"Computer wins,", computer_move, "beats", user_move)
-
-    if computer_choice == 2:
-        if user_input == 2:
-            print("Draw!")
-        elif user_input == 3:
-            print(f"Player wins,", user_move, "beats", computer_move)
-        elif user_input == 1:
-            print(f"Computer wins,", computer_move, "beats", user_move)
-
-    if computer_choice == 3:
-        if user_input == 3:
-            print("Draw!")
-        elif user_input == 1:
-            print(f"Player wins,", user_move, "beats", computer_move)
-        elif user_input == 2:
-            print(f"Computer wins,", computer_move, "beats", user_move)
+    if (user_move, computer_move) in [("Rock", "Scissors"), ("Scissors", "Paper"), ("Paper", "Rock")]:
+        print(f"Player wins, {user_move} beats {computer_move}")
+    elif user_move == computer_move:
+        print("Draw!")
+    else:
+        print(f"Computer wins, {computer_move} beats {user_move}")
 
 
 
